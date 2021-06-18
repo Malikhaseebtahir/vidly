c01ef8ac6c1e4e579bcbd3c57a623e5f9d1e5f84
Malik Haseeb <malikhaseeb1935@gmail.com>
2021-06-18 23:58:56 +0500

    Added the routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import './App.css';
+import { Route, Redirect, Switch } from 'react-router-dom';
 import Movies from './components/movies.component';
+import Home from './components/home';
+import Customers from './components/customers';
+import Rentals from './components/rentals';
+import Navbar from './components/navbar';
+import NotFound from './components/not-found';
+import MovieDetails from './components/movieDetails';
 
 function App() {
   return (
     <React.Fragment>
-      <div className="container p-5">
-        <Movies/>
+      <div>
+        <Navbar/>
+        <main className="container p-5">
+          <Switch>
+            <Route path="/movies/:id" component={MovieDetails}/>
+            <Route path="/movies" component={Movies}/>
+            <Route path="/customers" component={Customers}/>
+            <Route path="/rentals" component={Rentals}/>
+            <Route path="/not-found" component={NotFound}/>
+            <Redirect from="/" to="/movies" exact component={Movies}/>
+            <Redirect to="/not-found"/>
+          </Switch>
+        </main>
       </div>
     </React.Fragment>
   );
