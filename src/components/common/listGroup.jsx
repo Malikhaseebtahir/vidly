0f28225e02feb5a097c1612b69fcbf77d4214694
Malik Haseeb <malikhaseeb1935@gmail.com>
2021-07-11 12:29:21 +0500

    Converted the ListGroup class component into function component

--- src/components/common/listGroup.jsx
+++ src/components/common/listGroup.jsx
@@ -1,30 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 // Input => items: {},
 // Output => onItemSelect: func
-class ListGroup extends Component {
-    render() { 
-        const { 
-            items, 
-            valueProperty, 
-            textProperty, 
-            selectedGenre,
-            onItemSelect } = this.props;
-
-        return ( 
-            <ul className="list-group">
-                { items.map(item => 
-                    <li 
-                        key={item[valueProperty]} 
-                        className={ selectedGenre === item ? 'list-group-item active' : 'list-group-item' }
-                        onClick={() => onItemSelect(item)}>
-                        {item[textProperty]}
-                    </li>
-                )}
-            </ul> 
-        );
-    }
+const ListGroup = ({ items,valueProperty, textProperty, selectedGenre, onItemSelect }) => {
+    return ( 
+        <ul className="list-group">
+            { items.map(item => 
+                <li 
+                    key={item[valueProperty]} 
+                    className={ selectedGenre === item ? 'list-group-item active' : 'list-group-item' }
+                    onClick={() => onItemSelect(item)}>
+                    {item[textProperty]}
+                </li>
+            )}
+        </ul> 
+    );
 }
 
 ListGroup.defaultProps = {
