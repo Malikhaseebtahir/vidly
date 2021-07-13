46fdf47a374eebba8d3d60dc9b1d78cd2797d6b2
Malik Haseeb <malikhaseeb1935@gmail.com>
2021-07-14 01:40:11 +0500

    done refactoring in movieService.js file and remove unused files

--- src/services/moviesService.js
+++ src/services/moviesService.js
@@ -3,28 +3,29 @@ import { apiUrl } from '../config.json';
 
 const apiEndpoints = apiUrl + '/movies';
 
+function movieUrl(id) {
+    return `${apiEndpoints}/${id}`;
+}
+
 export function getMovies() {
     return http.get(apiEndpoints);
 }
   
 export function getMovie(id) {
-    return http.get(apiEndpoints + '/' + id);
+    return http.get(movieUrl(id));
 }
   
 export function saveMovie(movie) {
     if (movie._id) {
         const body = {...movie}
         delete body._id;
-        return http.put(apiEndpoints + '/' + movie._id, body);
+        return http.put(movieUrl(movie._id), body);
     }
     
     return http.post(apiEndpoints, movie);
 }
 
 export function deleteMovie(id) {
-    return http.delete(apiEndpoints + '/' + id);
-    // let movieInDb = movies.find(m => m._id === id);
-    // movies.splice(movies.indexOf(movieInDb), 1);
-    // return movieInDb;
+    return http.delete(movieUrl(id));
 }
   
