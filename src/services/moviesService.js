7f881f512bca8ad8c8a02d6f324ef6f40021ff65
Malik Haseeb <malikhaseeb1935@gmail.com>
2021-07-14 01:35:12 +0500

    Added the getMovies and saveMovies operations in moviesService

--- src/services/moviesService.js
+++ src/services/moviesService.js
@@ -7,24 +7,19 @@ export function getMovies() {
     return http.get(apiEndpoints);
 }
   
-// export function getMovie(id) {
-//     return movies.find(m => m._id === id);
-// }
+export function getMovie(id) {
+    return http.get(apiEndpoints + '/' + id);
+}
   
-//   export function saveMovie(movie) {
-//     let movieInDb = movies.find(m => m._id === movie._id) || {};
-//     movieInDb.title = movie.title;
-//     movieInDb.genre = genresAPI.genres.find(g => g._id === movie.genreId);
-//     movieInDb.numberInStock = movie.numberInStock;
-//     movieInDb.dailyRentalRate = movie.dailyRentalRate;
-  
-//     if (!movieInDb._id) {
-//       movieInDb._id = Date.now().toString();
-//       movies.push(movieInDb);
-//     }
-  
-//     return movieInDb;
-//   }
+export function saveMovie(movie) {
+    if (movie._id) {
+        const body = {...movie}
+        delete body._id;
+        return http.put(apiEndpoints + '/' + movie._id, body);
+    }
+    
+    return http.post(apiEndpoints, movie);
+}
 
 export function deleteMovie(id) {
     return http.delete(apiEndpoints + '/' + id);
